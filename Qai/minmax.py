7d8a86ac9250b19583a522477c4fbb625e08e176
Diego Barrales <diego.barrales@csu.fullerton.edu>
2024-03-14 16:08:53 -0700

    Added Qai folder for ai algorithms

---
+++ Qai/minmax.py
@@ -0,0 +1,8 @@
+
+class MinMaxPlayer():
+    def __init__(self, game):
+        self.game = game
+    
+    def play(self, board):
+        # TODO: create minmax algorithm for Qubic game
+        pass 

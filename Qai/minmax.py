8c41fe0d0854ccf552ed4a43214b9eaef5799926
Diego Barrales <diego.barrales@csu.fullerton.edu>
2024-03-15 12:36:07 -0700

    Skeleton code for MinMax algorithm, Without depth limiting

--- Qai/minmax.py
+++ Qai/minmax.py
@@ -1,8 +1,86 @@
+from random import randint
 
-class MinMaxPlayer():
+class MinMaxPlayer:
     def __init__(self, game):
         self.game = game
+
+    def has_filled_board(self, board):
+        valid_moves = self.game.getValidMoves(board, 1)
+        for x in valid_moves:
+            # there is still moves to be played 
+            if x:
+                return x
+            
+        return False
     
+    def has_won(self, board, player):
+        # TODO: check all possible winning combinations
+        return randint(0, 1)
+
+    def minmax(self, board):
+        # determine if board if terminal state
+        if self.has_won(board, 1):
+            return 1
+        elif self.has_won(board, -1):
+            return -1
+        elif self.has_filled_board(board):
+            return 0
+        
+        empty_cells = [i for (i, valid) in enumerate(self.game.getValidMoves(board, 1)) if valid]
+        # otherwise determine if the current play is for minimizing or maximizing
+        #   in other word, is the current move the AI's or human move
+        
+        move = 1 # TODO: determine the correct move to filled the board with  
+
+        best_move_idx = None
+        for cell_idx in empty_cells:
+            current_state = board
+            board[cell_idx] = move
+            
+            current_move_idx = None
+            if move == 1:
+                # TODO: create max_value function
+                current_move_idx = self.max_value(board)
+            else:
+                # TODO: create min_value function and maybe name them better
+                current_move_idx = self.min_value(board)
+
+            if best_move_idx is None or best_move_idx < current_move_idx:
+                best_move_idx = current_move_idx
+
+            # change the board back to the current_state each iteration
+            board = current_state
+
+        # return value from either function: 
+        # if AI's, run max_value(board)
+        # if human's, run min_value(board)
+        return best_move_idx
+
     def play(self, board):
         # TODO: create minmax algorithm for Qubic game
-        pass 
+        # TODO: create copy of board to pass to minmax function
+        # get all empty cells in a list
+        # go through each cell and run minmax on each cell
+        # collect the best moves from each minimax and return that move?
+        
+        
+        # Generate the whole game tree to leaves
+
+        # Apply utility (payoff) function to leaves.
+
+        # Use DFS for expanding the tree.
+
+        # Back-up values from the leaves towards the root. A MAX node computes the maximum value from its child values. A MIN node
+        # computes the minimum value from its child values.
+
+        # When value reaches the root: optimal move is determined.
+        valid_moves = self.game.getValidMoves(board, 1)
+        print("\nMoves:", [i for (i, valid) in enumerate(valid_moves) if valid])
+
+        while True:
+            move = int(input())
+            if valid_moves[move]:
+                break
+            else:
+                print("Invalid move")
+        return move
--- main.py
+++ main.py
@@ -1,64 +1,67 @@
-import pygame
+# import pygame
 
-pygame.init()
+# pygame.init()
 
-fps = 60
-fpsClock = pygame.time.Clock()
+# fps = 60
+# fpsClock = pygame.time.Clock()
 
-width, height = 640, 480
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Qubic")
+# width, height = 640, 480
+# screen = pygame.display.set_mode((width, height))
+# pygame.display.set_caption("Qubic")
 
-def displayGame(screen):
-    bottom_panel_height = 125
-    side_panel_width  = 150
-    screen_width, screen_height = pygame.display.get_window_size()
+# def displayGame(screen):
+#     bottom_panel_height = 125
+#     side_panel_width  = 150
+#     screen_width, screen_height = pygame.display.get_window_size()
 
-    main_panel_width = screen_width - side_panel_width
-    main_panel_height = screen_height - bottom_panel_height
+#     main_panel_width = screen_width - side_panel_width
+#     main_panel_height = screen_height - bottom_panel_height
 
-    displayMainPanel(screen, main_panel_width, main_panel_height)
-    displaySidePanel(screen, side_panel_width, main_panel_height)
-    displayBottomPanel(screen, bottom_panel_height)
-
-
-def displayMainPanel(screen, width, height):
-    pygame.draw.rect(screen, pygame.Color(0, 255, 0), (0, 0, width, height))
-
-def displaySidePanel(screen, width, height):
-    screen_width, _ = pygame.display.get_window_size()
-    pygame.draw.rect(screen, pygame.Color(255, 0, 0), (screen_width - width, 0, screen_width, height))
-
-def displayBottomPanel(screen, size):
-    width, height = pygame.display.get_window_size()
-    pygame.draw.rect(screen, pygame.Color(0, 0, 255), (0, height - size, width, height))
-
-running = True
-while running:
-    screen.fill((0, 0, 0))
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    displayGame(screen)
-
-    fpsClock.tick(fps)
-    pygame.display.update()
-
-pygame.quit()
+#     displayMainPanel(screen, main_panel_width, main_panel_height)
+#     displaySidePanel(screen, side_panel_width, main_panel_height)
+#     displayBottomPanel(screen, bottom_panel_height)
 
 
-# from GameLogic.qubic import QubicGame, display
-# from GameLogic.player import HumanQubicPlayer
-# from GameLogic.arena import Arena
+# def displayMainPanel(screen, width, height):
+#     pygame.draw.rect(screen, pygame.Color(0, 255, 0), (0, 0, width, height))
 
-# game = QubicGame(4, 4, 4)
+# def displaySidePanel(screen, width, height):
+#     screen_width, _ = pygame.display.get_window_size()
+#     pygame.draw.rect(screen, pygame.Color(255, 0, 0), (screen_width - width, 0, screen_width, height))
+
+# def displayBottomPanel(screen, size):
+#     width, height = pygame.display.get_window_size()
+#     pygame.draw.rect(screen, pygame.Color(0, 0, 255), (0, height - size, width, height))
+
+# running = True
+# while running:
+#     screen.fill((0, 0, 0))
+
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
+
+#     displayGame(screen)
+
+#     fpsClock.tick(fps)
+#     pygame.display.update()
+
+# pygame.quit()
+
+
+from GameLogic.qubic import QubicGame, display
+from GameLogic.player import HumanQubicPlayer
+from GameLogic.arena import Arena
+
+from Qai.minmax import MinMaxPlayer
+
+game = QubicGame(4, 4, 4)
 
 # player1 = HumanQubicPlayer(game).play
-# player2 = HumanQubicPlayer(game).play
+player1 = MinMaxPlayer(game).play
+player2 = HumanQubicPlayer(game).play
 
-# player_list = [player1, player2]
+player_list = [player1, player2]
 
-# arena = Arena(player1, player2, game, display=display)
-# print(arena.playGame(verbose=True))
+arena = Arena(player1, player2, game, display=display)
+print(arena.playGame(verbose=True))
